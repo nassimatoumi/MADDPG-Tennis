62595d5d639245cb62532e79702daa8446c5691b
TOUMI Nassima <toumi.nassi@gmail.com>
2021-11-05 12:43:11 +0100

    Update use_agent.py

--- use_agent.py
+++ use_agent.py
@@ -1,11 +1,11 @@
 from unityagents import UnityEnvironment
 import numpy as np
 from collections import deque
-from dqn_agent import Agent
+from ddpg_agent import Agent
 import matplotlib.pyplot as plt
 import torch
 
-env = UnityEnvironment(file_name='/data/Reacher_Linux_NoVis/Reacher.x86_64')
+env = UnityEnvironment(file_name="/data/Tennis_Linux_NoVis/Tennis")
 num_episodes = 10
 
 # get the default brain
@@ -24,7 +24,7 @@ print('Number of actions:', action_size)
 # examine the state space
 states = env_info.vector_observations
 print('States look like:', states)
-state_size = len(states)
+state_size = states.shape[1]
 print('States have length:', state_size)
 
 agent = Agent(state_size, action_size, 0)
@@ -34,15 +34,13 @@ if torch.cuda.is_available():
 else:
     map_location='cpu'
 
-    
 agent.actor_local.load_state_dict(torch.load('actor_checkpoint.pth',map_location=map_location))
 agent.critic_local.load_state_dict(torch.load('critic_checkpoint.pth',map_location=map_location))
 
 for i in range(num_episodes):
-    # for i_episode in range(1, n_episodes + 1):
     env_info = env.reset(train_mode=False)[brain_name]
     states = env_info.vector_observations
-    score = 0
+    score = np.zeros(len(env_info.agents))
     while True:
         actions = agent.act(states)
         actions = np.clip(actions, -1, 1)  # all actions between -1 and 1
@@ -51,10 +49,11 @@ for i in range(num_episodes):
         rewards = env_info.rewards  # get the reward
         dones = env_info.local_done  # see if episode has finished
         states = next_states
-        score += reward
+        score += rewards
         if np.any(dones):  # exit loop if episode finished
             break
-
-    print('\rEpisode {}\tScore: {:.2f}'.format(i, score))
+    print("Episode "+str(i))
+    print("Maximum score "+str(np.max(score)))
+    print('\rEpisode {}\tMaximum Score: {:.2f}'.format(i, np.max(score)), end="")
 
 env.close()
